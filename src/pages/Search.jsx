3ad355a69c70968d25b904cf570c5aafa4209b54
polidex <lp.igor@gmail.com>
2022-05-24 22:36:25 -0300

    5 done

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,34 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    inputArtist: '',
+    isDisabled: false,
+  }
+
+  handleChange = ({ target: { value } }) => {
+    const inputMin = 2;
+    this.setState({ inputArtist: value });
+    this.setState({ isDisabled: value.length >= inputMin });
+  }
+
   render() {
+    const { inputArtist, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
-        <Header>
-          <h1>Search</h1>
-        </Header>
+        <Header />
+        <input
+          data-testid="search-artist-input"
+          value={ inputArtist }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ !isDisabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
