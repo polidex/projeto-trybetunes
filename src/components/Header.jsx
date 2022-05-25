cab18f4e10d99c584da3f5b9e38328768187e8cd
polidex <lp.igor@gmail.com>
2022-05-24 22:14:21 -0300

    4 done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -19,7 +20,17 @@ export default class Header extends React.Component {
     return (
       <header data-testid="header-component">
         { isLoading
-          ? <Loading /> : <h1 data-testid="header-user-name">{ user.name }</h1> }
+          ? <Loading />
+          : (
+            <div>
+              <h1 data-testid="header-user-name">
+                { user.name }
+              </h1>
+              <Link data-testid="link-to-search" to="/search">Pesquisa</Link>
+              <Link data-testid="link-to-favorites" to="/favorites">Favoritas</Link>
+              <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+            </div>
+          )}
       </header>
     );
   }
